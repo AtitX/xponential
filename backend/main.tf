7831b88680e3fdf5fa3dc5886318d9edbfb25dc1
Atit <atit.r@taskworld.com>
2023-09-08 14:10:44 +0700

    Remove unwanted file

--- backend/main.tf
+++ backend/main.tf
@@ -15,18 +15,6 @@ resource "random_id" "id" {
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "terraform-state-${random_id.id.hex}"
   force_destroy = true
-  # versioning {
-  #   enabled = true
-  # }
-
-  # Enable server-side encryption by default
-  # server_side_encryption_configuration {
-  #   rule {
-  #     apply_server_side_encryption_by_default {
-  #       sse_algorithm = "AES256"
-  #     }
-  #   }
-  # }
 }
 
 resource "aws_kms_key" "s3key" {
